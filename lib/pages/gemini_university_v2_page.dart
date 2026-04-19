cfe5d838a976f0f8ceefd4fdb5a35af061e9d9d4
kanta13jp1 <kanta13jp1@users.noreply.github.com>
2026-04-19 19:39:30 +0900

    fix: add missing kIsWeb import in gemini_university_v2_page.dart (CI #522-524)

--- lib/pages/gemini_university_v2_page.dart
+++ lib/pages/gemini_university_v2_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:math' show Random;
 import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
