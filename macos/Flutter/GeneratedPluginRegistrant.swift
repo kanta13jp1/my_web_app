76acaec362e726a8abdbd1df72c3d0ca4f493a48
kanta13jp1 <77245414+kanta13jp1@users.noreply.github.com>
2026-05-01 12:24:03 +0900

    Fix mobile release artifact build dependencies
    Removes unused mobile-breaking dependencies so Android AAB and iOS no-codesign artifacts can build in GitHub Actions. Progress toward #1495.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import app_links
 import audioplayers_darwin
-import device_info_plus
 import file_picker
 import file_selector_macos
 import flutter_local_notifications
@@ -22,7 +21,6 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
