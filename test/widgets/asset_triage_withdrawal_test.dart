19ce5769535c91b34ddc57bb1c0fd6c6c8059589
kanta13jp1 <77245414+kanta13jp1@users.noreply.github.com>
2026-09-13 20:48:54 +0900

    test: diagnose triage VM overflow before full browser validation

--- test/widgets/asset_triage_withdrawal_test.dart
+++ test/widgets/asset_triage_withdrawal_test.dart
@@ -1,4 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
 import '../../integration_test/asset_triage_withdrawal_scenarios.dart'
     as scenarios;
 
-void main() => scenarios.main();
+class _DiagnosticBinding extends AutomatedTestWidgetsFlutterBinding {
+  @override
+  void reportExceptionNoticed(FlutterErrorDetails exception) {
+    debugPrint(exception.toString());
+    super.reportExceptionNoticed(exception);
+  }
+}
+
+void main() {
+  _DiagnosticBinding();
+  scenarios.main();
+}
