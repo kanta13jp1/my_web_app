ebb1625ad45a6332a7214e1d5c93ac12ac945feb
kanta13jp1 <77245414+kanta13jp1@users.noreply.github.com>
2026-09-13 20:08:06 +0900

    test: preserve first triage browser framework exception

--- integration_test/asset_triage_withdrawal_test.dart
+++ integration_test/asset_triage_withdrawal_test.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:integration_test/integration_test.dart';
+
 import 'asset_triage_withdrawal_scenarios.dart' as scenarios;
+
+class _DiagnosticBinding extends IntegrationTestWidgetsFlutterBinding {
+  @override
+  void reportExceptionNoticed(FlutterErrorDetails exception) {
+    final data = reportData ??= <String, dynamic>{};
+    data.putIfAbsent('firstFrameworkError', () => exception.toString());
+    super.reportExceptionNoticed(exception);
+  }
+}
 
 // Synthetic persistence boundaries; no authenticated backend or real withdrawal.
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  _DiagnosticBinding();
   scenarios.main();
 }
